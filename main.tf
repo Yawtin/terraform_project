42a633849c0944f50add42720ed34fbef6670d83
Yatin Arora <yatinarora9729@gmail.com>
2024-04-27 14:26:36 +0530

    Commit 1

---
+++ main.tf
@@ -0,0 +1,165 @@
+resource "aws_vpc" "myvpc" {
+  cidr_block = var.cidr
+}
+
+resource "aws_subnet" "sub1" {
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "sub2" {
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "myig" {
+  vpc_id = aws_vpc.myvpc.id
+}
+
+resource "aws_route_table" "myrt" {
+  vpc_id = aws_vpc.myvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myig.id
+  }
+}
+
+resource "aws_route_table_association" "rta1" {
+  subnet_id      = aws_subnet.sub1.id
+  route_table_id = aws_route_table.myrt.id
+}
+
+resource "aws_route_table_association" "rta2" {
+  subnet_id      = aws_subnet.sub2.id
+  route_table_id = aws_route_table.myrt.id
+}
+
+resource "aws_security_group" "mysg" {
+  name        = "web-sg"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.myvpc.id
+
+  tags = {
+    Name = "mysg"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.mysg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.mysg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.mysg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+resource "aws_s3_bucket" "mys3bucket" {
+  bucket = "mujheneendnaaaye"
+}
+
+resource "aws_s3_bucket_ownership_controls" "mys3bucket" {
+  bucket = aws_s3_bucket.mys3bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "mys3bucket" {
+  bucket = aws_s3_bucket.mys3bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "mys3bucket" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.mys3bucket,
+    aws_s3_bucket_public_access_block.mys3bucket,
+  ]
+
+  bucket = aws_s3_bucket.mys3bucket.id
+  acl    = "public-read"
+}
+
+resource "aws_instance" "myec1" {
+  ami                    = "ami-080e1f13689e07408"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.mysg.id]
+  subnet_id              = aws_subnet.sub1.id
+  user_data              = base64encode(file("userdata.sh"))
+}
+
+resource "aws_instance" "myec2" {
+  ami                    = "ami-080e1f13689e07408"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.mysg.id]
+  subnet_id              = aws_subnet.sub2.id
+  user_data              = base64encode(file("userdata1.sh"))
+}
+
+resource "aws_lb" "myalb" {
+  name               = "myalb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.mysg.id]
+  subnets            = [aws_subnet.sub1.id, aws_subnet.sub2.id]
+}
+
+resource "aws_lb_target_group" "mytg" {
+  name     = "mytg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.myvpc.id
+
+  health_check {
+    path = "/"
+    port = "traffic-port"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "attach1" {
+  target_group_arn = aws_lb_target_group.mytg.arn
+  target_id        = aws_instance.myec1.id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "attach2" {
+  target_group_arn = aws_lb_target_group.mytg.arn
+  target_id        = aws_instance.myec2.id
+  port             = 80
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.myalb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.mytg.arn
+  }
+}
+
+output "loadbalancerdns" {
+  value = aws_lb.myalb.dns_name
+}
